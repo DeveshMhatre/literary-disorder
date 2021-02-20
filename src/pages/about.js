901f1d61704fe5c9f3337ab4c79d2992651a1a58
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-02-20 12:00:23 +0530

    Add About and Contact pages

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <article className="about">
+        <p className="about__heading">About</p>
+      </article>
+    </Layout>
+  )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const About = () => {
+  return (
+    <Layout>
+      <article className="contact">
+        <p className="contact">Contact</p>
+      </article>
+    </Layout>
+  )
+}
+
+export default About
