544519515d44148d3f2142fb30d063a10c4fdbc2
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-02-24 09:39:08 +0530

    Show all posts in Home page
    - Create PostLink component for posts in Home
    - Finish UI for PostLink component
    - Finish UI for Home page

---
+++ src/components/post-link.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+
+const PostLink = ({ post }) => (
+  <section className="post-link">
+    <h3 className="post-link__title">{post.frontmatter.title}</h3>
+    <p className="post-link__meta post-meta">
+      {post.frontmatter.date}
+      <span className="post-meta__separator">&#9679;</span>{' '}
+      {post.frontmatter.readingTime} Min Read
+    </p>
+    <p className="post-link__desc">{post.frontmatter.description}</p>
+    <p className="post-link__link">
+      <Link className="post-link__link--text" to={post.frontmatter.slug}>
+        Continue reading <i className="gg-arrow-long-right"></i>
+      </Link>
+    </p>
+  </section>
+)
+
+PostLink.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    frontmatter: PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      readingTime: PropTypes.number.isRequired,
+    }),
+  }),
+}
+
+export default PostLink
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,65 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
+import PostLink from '../components/post-link'
 
-const HomePage = () => {
+const HomePage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  // All the posts in PostLink component
+  const Posts = edges
+    .filter((edge) => !!edge.node.frontmatter.date)
+    .map((edge) => <PostLink key={edge.node.id} post={edge.node} />)
+
   return (
     <Layout>
-      <article className="main home">
-        <h1 className="home__heading">Hello World!</h1>
-      </article>
+      <article className="main home">{Posts}</article>
     </Layout>
   )
 }
 
+HomePage.propTypes = {
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            id: PropTypes.string.isRequired,
+            frontmatter: PropTypes.shape({
+              date: PropTypes.string.isRequired,
+              slug: PropTypes.string.isRequired,
+              title: PropTypes.string.isRequired,
+              readingTime: PropTypes.number.isRequired,
+              description: PropTypes.string.isRequired,
+            }),
+          }),
+        }),
+      ),
+    }),
+  }),
+}
+
 export default HomePage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+            readingTime
+            description
+          }
+        }
+      }
+    }
+  }
+`
