eda0040a85d8a1830327182df2e7e2177a088db0
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-02-27 09:54:14 +0530

    Prevent access to some pages (404, thankyou) without redirect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { navigate } from 'gatsby'
 
 import ThemeContextProvider from './src/contexts/ThemeContext'
 
 import './src/styles/index.scss'
 
+// Global context provider
 export const wrapRootElement = ({ element }) => (
   <ThemeContextProvider>{element}</ThemeContextProvider>
 )
+
+// Prevent navigating to these pages unless redirected
+export const onInitialClientRender = () => {
+  if (
+    window.location.pathname.startsWith('/thankyou') ||
+    window.location.pathname.startsWith('/404')
+  ) {
+    navigate('/')
+  }
+}
