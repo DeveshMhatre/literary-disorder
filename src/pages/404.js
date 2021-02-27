28e844b87131c7354269d62f91a511bb961461c7
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2021-02-27 10:07:02 +0530

    Style 404 page
    - Remove individual styles for 404, thankyou pages
    - Add info-page styles for 404, thankyou pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Layout from '../components/layout'
+
 const PageNotFound = () => {
   return (
-    <main className="not-found">
-      Could not find the page you are looking for!
-      <Link to="/">Go back</Link>
-    </main>
+    <Layout>
+      <article className="main info-page">
+        <h3 className="info-page__heading">
+          Could not find the page you are looking for&hellip;
+        </h3>
+        <Link to="/" className="info-page__link">
+          Go back
+        </Link>
+      </article>
+    </Layout>
   )
 }
 
--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -5,12 +5,12 @@ import Layout from '../components/layout'
 
 const ThankYou = () => (
   <Layout>
-    <article className="main thank-you">
-      <h3 className="thank-you__heading">Thank you for getting in touch!</h3>
-      <p className="thank-you__message">
+    <article className="main info-page">
+      <h3 className="info-page__heading">Thank you for getting in touch!</h3>
+      <p className="info-page__message">
         Your message has been successfully sent! I will contact you soon!
       </p>
-      <Link to="/contact" className="thank-you__link">
+      <Link to="/contact" className="info-page__link">
         Go back
       </Link>
     </article>
